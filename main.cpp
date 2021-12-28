d852f878da66a678c9443f9685841acf4d1be279
Grzegorz Biskowski <biskowski.grzegorz@gmail.com>
2021-12-28 18:20:45 +0100

    Translated exercises to english

--- main.cpp
+++ main.cpp
@@ -6,177 +6,177 @@
 
 using namespace std;
 
-int cwiczenie_nr2_1();
-int cwiczenie_nr2_2();
-int cwiczenie_nr2_3();
-int cwiczenie_nr3_1();
-int cwiczenie_nr3_2();
-int cwiczenie_nr3_3();
-int cwiczenie_nr4_1();
-int cwiczenie_nr4_2();
-int cwiczenie_nr4_3();
-int cwiczenie_nr4_4();
-int cwiczenie_nr4_5();
+int exercise_no2_1();
+int exercise_no2_2();
+int exercise_no2_3();
+int exercise_no3_1();
+int exercise_no3_2();
+int exercise_no3_3();
+int exercise_no4_1();
+int exercise_no4_2();
+int exercise_no4_3();
+int exercise_no4_4();
+int exercise_no4_5();
 bool Prime(unsigned int nNumber);
 
 int main()
 {
-    cout << "Cwiczenia z ksiazki od zera do gier kodera:" << endl;
+    cout << "Exercises for course by Karol Kuczmarski (Xion):" << endl;
     cout << "==========================================" << endl;
-    cout << "1. Cwiczenie 2.1. - Oczekiwanie na klawisz po wypisaniu w konsoli linijki tekstu" << endl;
-    cout << "2. Cwiczenie 2.2. - Program wyswietlajacy imie i nazwisko po ich wpisaniu do konsoli" << endl;
-    cout << "3. Cwiczenie 2.3. - Iloczyn trzech liczb" << endl;
-    cout << "4. Cwiczenie 3.1. - Klasyfikacja wpisanej liczby" << endl;
-    cout << "5. Cwiczenie 3.2. - Liczby od 1 do 100 i ich drugie potegi" << endl;
-    cout << "6. Cwiczenie 3.3. - Gra w odgadniecie wylosowanej liczby" << endl;
-    cout << "7. Cwiczenie 4.1. - Rozrzut zmiennych int i unsigned int" << endl;
-    cout << "8. Cwiczenie 4.2. - Sizeof dla nowych nazw typu short int i long int" << endl;
-    cout << "9. Cwiczenie 4.3. - Tabela znakow ANSI wraz z zatrzymaniem po zapelnieniu konsoli" << endl;
-    cout << "10. Cwiczenie 4.4. - Rozwiazywanie rownan kwadratowych" << endl;
-    cout << "11. Cwiczenie 4.5. - Sprawdzanie czy wpisana liczba jest liczba pierwsza" << endl;
-    cout << "0. Wyjdz" << endl;
+    cout << "1. Exercise 2.1. - Waiting for a character after writing a line of text" << endl;
+    cout << "2. Exercise 2.2. - Writing first name and last name in a full sentence" << endl;
+    cout << "3. Exercise 2.3. - Multiplying three number" << endl;
+    cout << "4. Exercise 3.1. - Classifying given number" << endl;
+    cout << "5. Exercise 3.2. - Numbers from 1 to 100 and their squares" << endl;
+    cout << "6. Exercise 3.3. - Guess the number game" << endl;
+    cout << "7. Exercise 4.1. - Span of int and unsigned int" << endl;
+    cout << "8. Exercise 4.2. - Sizeof for new names of short int and long int" << endl;
+    cout << "9. Exercise 4.3. - ANSI list with break after the console is full" << endl;
+    cout << "10. Exercise 4.4. - Solving quadratic equations" << endl;
+    cout << "11. Exercise 4.5. - Checking if a given number is prime" << endl;
+    cout << "0. Exit the program" << endl;
     int choise;
     while (true)
     {
-        cout << "Twoj wybor: ";
+        cout << "Your choise: ";
         cin >> choise;
         switch(choise)
         {
-            case 1: cwiczenie_nr2_1(); break;
-            case 2: cwiczenie_nr2_2(); break;
-            case 3: cwiczenie_nr2_3(); break;
-            case 4: cwiczenie_nr3_1(); break;
-            case 5: cwiczenie_nr3_2(); break;
-            case 6: cwiczenie_nr3_3(); break;
-            case 7: cwiczenie_nr4_1(); break;
-            case 8: cwiczenie_nr4_2(); break;
-            case 9: cwiczenie_nr4_3(); break;
-            case 10: cwiczenie_nr4_4(); break;
-            case 11: cwiczenie_nr4_5(); break;
+            case 1: exercise_no2_1(); break;
+            case 2: exercise_no2_2(); break;
+            case 3: exercise_no2_3(); break;
+            case 4: exercise_no3_1(); break;
+            case 5: exercise_no3_2(); break;
+            case 6: exercise_no3_3(); break;
+            case 7: exercise_no4_1(); break;
+            case 8: exercise_no4_2(); break;
+            case 9: exercise_no4_3(); break;
+            case 10: exercise_no4_4(); break;
+            case 11: exercise_no4_5(); break;
             case 0: return 0; break;
-            default: cout << "Niepoprawna opcja" << endl; break;
+            default: cout << "Wrong input" << endl; break;
         }
     }
     return 0;
 }
 
-int cwiczenie_nr2_1()
+int exercise_no2_1()
 {
-    cout << "Trzy linijki tekstu:" << endl;
-    cout << "Pierwsza linijka tekstu" << endl;
+    cout << "Three lines of text:" << endl;
+    cout << "First line" << endl;
     getch();
-    cout << "Druga linijka tekstu" << endl;
+    cout << "Second line" << endl;
     getch();
-    cout << "Trzecia linijka tekstu" << endl;
+    cout << "Third line" << endl;
     getch();
     return 0;
 }
-int cwiczenie_nr2_2()
+int exercise_no2_2()
 {
-    string imie, nazwisko;
-    cout << "Aplikacja podajaca w pe³nym zdaniu imie i nazwisko uzytkownika" << endl;
-    cout << "Podaj swoje imie: ";
-    cin >> imie;
-    cout << "Podaj swoje nazwisko: ";
-    cin >> nazwisko;
-    cout << "Nazywasz sie " << imie << " " << nazwisko << "." << endl;
+    string strName, strLastname;
+    cout << "Program writing first name and last name in a full sentence" << endl;
+    cout << "Your name: ";
+    cin >> strName;
+    cout << "Your last name: ";
+    cin >> strName;
+    cout << "Your name is " << strName << " " << strLastname << "." << endl;
     return 0;
 }
-int cwiczenie_nr2_3()
+int exercise_no2_3()
 {
-    int a, b, c;
-    cout << "Aplikacja obliczajaca iloczyn trzech liczb" << endl;
-    cout << "Podaj pierwsza liczbe: ";
-    cin >> a;
-    cout << "Podaj druga liczbe: ";
-    cin >> b;
-    cout << "Podaj trzecia liczbe: ";
-    cin >> c;
-    cout << a << " * " << b << " * " << c << " = " << a*b*c << endl;
+    double dA, dB, dC;
+    cout << "Program that calculates a product of three numbers" << endl;
+    cout << "First number: ";
+    cin >> dA;
+    cout << "Second number: ";
+    cin >> dB;
+    cout << "Third number: ";
+    cin >> dC;
+    cout << dA << " * " << dB << " * " << dC << " = " << dA * dB * dC << endl;
     return 0;
 }
-int cwiczenie_nr3_1()
+int exercise_no3_1()
 {
-    int number;
-    cout << "Klasyfikacja liczby" << endl;
-    cout << "Podaj liczbe, a ja zaklasyfikuje ja do liczb ujemnych, dodatnich jednocyfrowych, dwucyfrowych lub pozostalych: " << endl;
-    cin >> number;
-    if (number < 0)
+    int nNumber;
+    cout << "Classification of a number" << endl;
+    cout << "Give me a number and I will classify it as a negative, positive one-digit, two-digit or another." << endl;
+    cin >> nNumber;
+    if (nNumber < 0)
     {
-        cout << "Liczba " << number << " jest liczba ujemna." << endl;
-    } else if (number >= 0 && number < 10)
+        cout << "Number " << nNumber << " is a negative number." << endl;
+    } else if (nNumber >= 0 && nNumber < 10)
     {
-        cout << "Liczba " << number << " jest dodatnia liczba jednocyfrowa." << endl;
-    } else if (number >= 10 && number < 100)
+        cout << "Number " << nNumber << " is a positive one-digit number." << endl;
+    } else if (nNumber >= 10 && nNumber < 100)
     {
-        cout << "Liczba " << number << " jest dodatnia liczba dwucyfrowa." << endl;
+        cout << "Number " << nNumber << " is a positive two-digit number." << endl;
     } else
     {
-        cout << "Liczba " << number << " nalezy do kategorii pozostalych liczb." << endl;
+        cout << "Number " << nNumber << " is classified as another type of numbers." << endl;
     }
     return 0;
 }
-int cwiczenie_nr3_2()
+int exercise_no3_2()
 {
-    cout << "Liczby od 1 do 100 oraz ich kwadrtaty" << endl;
+    cout << "Numbers from 1 to 100 and their squares" << endl;
     for (int i = 1; i <= 100; i +=2)
     {
         cout << i << " --- " << i*i << " || " << i+1 << " --- " << (i+1)*(i+1) << endl;
     }
     return 0;
 }
-int cwiczenie_nr3_3()
+int exercise_no3_3()
 {
-    cout << "Gra w odgadniecie wylosowanej liczby" << endl;
-    int random, guess = 0, guess_count = 1;
+    cout << "Guess a number game" << endl;
+    int nRandom, nGuess = 0, nGuess_count = 1;
     srand (time(NULL));
-    random = rand() % 100 + 1;
-    cout << "Wylosowalem pewna liczbe, sprobuj ja odgadnac: " << endl;
+    nRandom = rand() % 100 + 1;
+    cout << "I drew a number from 1 to 100. Try to guess it: " << endl;
     while (true)
     {
-        cin >> guess;
-        if (guess == random)
+        cin >> nGuess;
+        if (nGuess == nRandom)
         {
-            cout << "Gratulacje! Odgadles liczbe" << endl;
-            cout << "To byla twoja " << guess_count << " proba." << endl;
+            cout << "Congratulations! You guessed the correct number!" << endl;
+            cout << "It was your guess no. " << nGuess_count << endl;
             return 0;
-        } else if (guess < random)
+        } else if (nGuess < nRandom)
         {
-            cout << "Wylosowana liczba jest wieksza" << endl;
-            cout << "To byla twoja " << guess_count << " proba." << endl;
-            cout << "Sprobuj jeszcze raz: ";
-            guess_count++;
-        } else if (guess > random)
+            cout << "My number is greater than yours" << endl;
+            cout << "It was your guess no. " << nGuess_count << endl;;
+            cout << "Try again: ";
+            nGuess_count++;
+        } else if (nGuess > nRandom)
         {
-            cout << "Wylosowana liczba jest mniejsza" << endl;
-            cout << "To byla twoja " << guess_count << " proba." << endl;
-            cout << "Sprobuj jeszcze raz: ";
-            guess_count++;
+            cout << "My number is smaller than yours" << endl;
+            cout << "It was your guess no. " << nGuess_count << endl;;
+            cout << "Try again: ";
+            nGuess_count++;
         }
     }
     return 0;
 }
-int cwiczenie_nr4_1()
+int exercise_no4_1()
 {
-    cout << "Przypisanie wartosci 3 000 000 000 do zmiennej typu int i unsigned int" << endl;
-    int nZmienna = 3000000000;
-    unsigned int uZmienna = 3000000000;
-    cout << "Wartosc zmiennej typu int: " << nZmienna << endl;
-    cout << "Wartosc zmiennej typu unsigned int: " << uZmienna << endl;
+    cout << "Assigning value of 3,000,000,000 do to int and unsigned int" << endl;
+    int nVariable = 3000000000;
+    unsigned int uVariable = 3000000000;
+    cout << "Value of int variable: " << nVariable << endl;
+    cout << "Value of unsigned int variable: " << uVariable << endl;
     return 0;
 }
-int cwiczenie_nr4_2()
+int exercise_no4_2()
 {
-    cout << "Sizeof dla nowych nazw typu short int i long int" << endl;
-    typedef short int krotki;
-    typedef long int dlugi;
-    cout << "Typ short int nazwany krotki: " << sizeof(krotki) << endl;
-    cout << "Typ long int nazwany dlugi: " << sizeof(dlugi) << endl;
+    cout << "Sizeof for new names of short int and long int" << endl;
+    typedef short int shorter_variable;
+    typedef long int longer_variable;
+    cout << "short int called now shorter_variable - size: " << sizeof(shorter_variable) << endl;
+    cout << "long int called now longer_variable - size: " << sizeof(longer_variable) << endl;
     return 0;
 }
-int cwiczenie_nr4_3()
+int exercise_no4_3()
 {
-    cout << "Wypisanie w konsoli znakow ANSI wraz z ich indeksami" << endl;
+    cout << "Writing ANSI list in console with their indecies" << endl;
     for (int i = 32; i <= 256; i++)
     {
         if ((i-32)/29 > 0 && (i-32)%29 == 0)
@@ -187,46 +187,46 @@ int cwiczenie_nr4_3()
     }
     return 0;
 }
-int cwiczenie_nr4_4()
+int exercise_no4_4()
 {
-    double a, b, c, delta;
-    cout << "Rozwiazywanie rownan kwadratowych (tylko wynik rzeczywisty)";
-    cout << "Rownanie ma postac ax^2 + bx + c = 0" << endl;
-    cout << "Wpisz wspolczynnik a: ";
-    cin >> a;
-    cout << "Wpisz wspolczynnik b: ";
-    cin >> b;
-    cout << "Wpisz wspolczynnik c: ";
-    cin >> c;
-    if (a == 0)
+    double dA, dB, dC, dDelta;
+    cout << "Solving quadratic equations (only real solutions)";
+    cout << "Equation has a form of ax^2 + bx + c" << endl;
+    cout << "Write coefficient a: ";
+    cin >> dA;
+    cout << "Write coefficient b: ";
+    cin >> dB;
+    cout << "Write coefficient c: ";
+    cin >> dC;
+    if (dA == 0)
     {
-        if (b == 0)
+        if (dB == 0)
         {
-            if (c == 0)
+            if (dC == 0)
             {
-                cout << "Jest to linia prosta y = 0, dla ktorej rownanie ma nieskonczenie wiele rozwiazan." << endl;
+                cout << "It is straight line y = 0, for which the equation has infinitely many solutions." << endl;
             } else
             {
-                cout << "Rownanie nie ma rozwiazan." << endl;
+                cout << "The equation does not have any solution." << endl;
             }
         } else
         {
-            cout << "Jest to rownanie liniowe z miejscem zerowym w punkcie x = " << -c/b << endl;
+            cout << "It is a linear equation with y = 0 for x = " << -dC/dB << endl;
         }
     } else
     {
-        delta = (b*b)-(4 * a * c);
-        if (delta < 0)
+        dDelta = (dB*dB)-(4 * dA * dC);
+        if (dDelta < 0)
         {
-            cout << "Rownanie nie ma rozwiazan rzeczywistych." << endl;
-        } else if (delta == 0)
+            cout << "The equation does not have any real solution" << endl;
+        } else if (dDelta == 0)
         {
-            cout << "Rownanie ma jedno rozwiazanie rzeczywiste w punkcie x = " << (-b)/(2 * a) << endl;
+            cout << "The equation has one real solution in x = " << (-dB)/(2 * dA) << endl;
         } else
         {
-            cout << "Rownanie ma nastepujace rownania:" << endl;
-            cout << "x1 = " << (-b - sqrt(delta))/(2 * a) << endl;
-            cout << "x2 = " << (-b + sqrt(delta))/(2 * a) << endl;
+            cout << "The equation has real solutions in:" << endl;
+            cout << "x1 = " << (-dB - sqrt(dDelta))/(2 * dA) << endl;
+            cout << "x2 = " << (-dB + sqrt(dDelta))/(2 * dA) << endl;
         }
     }
     return 0;
@@ -246,12 +246,12 @@ bool Prime(unsigned int nNumber)
     }
     return true;
 }
-int cwiczenie_nr4_5()
+int exercise_no4_5()
 {
     unsigned int nNumber;
-    cout << "Sprawdzanie czy wpisana liczba jest liczba pierwsza" << endl;
-    cout << "Wpisz pewna liczbe, a ja sprawdze czy jest liczba pierwsza: ";
+    cout << "Checking if a given number is prime." << endl;
+    cout << "Write a number and I will check if it is prime: ";
     cin >> nNumber;
-    cout << (Prime(nNumber) ? "Liczba jest liczba pierwsza." : "Liczba nie jest liczba pierwsza.") << endl;
+    cout << (Prime(nNumber) ? "Your number IS prime." : "Your number IS NOT prime.") << endl;
     return 0;
 }
